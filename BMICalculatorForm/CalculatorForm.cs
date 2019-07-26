10dc69ffe87301d67c2a14a896f8b67157a8ebe9
Tanay Kapoor <kapoortanay15@gmail.com>
2019-07-26 09:20:10 -0400

    Added Code To put Inputs

--- BMICalculatorForm/CalculatorForm.cs
+++ BMICalculatorForm/CalculatorForm.cs
@@ -12,9 +12,36 @@ namespace BMICalculatorForm
 {
     public partial class CalculatorForm : Form
     {
+        /// <summary>
+        /// This is the constructor for the CalculatorForm 
+        /// </summary>
         public CalculatorForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is a Shared event handler for the CalculatorButton Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            var TheButton = sender as Button;
+
+            int ButtonValue;
+            bool Result = int.TryParse(TheButton.Text, out ButtonValue);
+
+            if (Result)
+            {
+
+                ResultLabel.Text = TheButton.Text;
+
+            }
+            else
+            {
+                ResultLabel.Text = "Not a Number";
+            }
         }
     }
 }
