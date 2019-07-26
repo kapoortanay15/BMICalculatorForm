706cd45d8b3d628900fca6a824ba09fb1a5a79c8
Tanay Kapoor <kapoortanay15@gmail.com>
2019-07-26 07:10:12 -0400

    Created a base form for BMI Calculator

--- BMICalculatorForm/Program.cs
+++ BMICalculatorForm/Program.cs
@@ -16,7 +16,7 @@ namespace BMICalculatorForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CalculatorForm());
         }
     }
 }
